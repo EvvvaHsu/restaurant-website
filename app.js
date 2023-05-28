72e4515e86e38feb1c66634027c75e36e53fccf2
Yiching Hsu <ychsu7149@gmail.com>
2023-05-28 21:20:30 +0800

    Modify search function

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const port = 3000
 
 // require express-handlebars here
 const exphbs = require('express-handlebars')
-const restaurantList = require('./restaurant.json')
+const restaurantList = require('./restaurant.json').results
 
 // setting template engine
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
@@ -20,7 +20,7 @@ app.use(express.static('public'))
 // routes setting
 app.get('/', (req, res) => {
 
-    res.render('index', {restaurants: restaurantList.results})
+    res.render('index', { restaurants: restaurantList })
 })
 
 
@@ -29,23 +29,28 @@ app.get('/restaurants/:restaurant_id', (req, res) => {
 
     //console.log(req.params.restaurant_id)
 
-    const restaurant = restaurantList.results.find(
+    const restaurant = restaurantList.find(
         restaurant => restaurant.id.toString() === req.params.restaurant_id
     )
-    res.render('show', {restaurant: restaurant})
+    res.render('show', { restaurant: restaurant })
 })
 
 app.get('/search', (req, res) => {
     //console.log(req)
-    const keyword = req.query.keyword
-    const restaurants = restaurantList.results.filter(restaurant => {
-        return restaurant.name.toLowerCase().includes(keyword.toLowerCase())
+    if (!req.query.keyword) {
+        return res.redirect("/")
+    }
+    const keyword = req.query.keyword.trim().toLowerCase()
+
+    const filterRestaurants = restaurantList.filter(data => {
+        return data.name.toLowerCase().includes(keyword) || data.category.includes(keyword)
     })
-    res.render('index', {restaurants: restaurants, keyword: keyword })
+    
+    res.render('index', { restaurants: filterRestaurants, keyword: keyword })
 })
 
 
 // start and listen on the Express server
 app.listen(port, () => {
-    console.log(`Express is listening on localhost:${port}`)
+    console.log(`Express is listening on http://localhost:${port}`)
 })
